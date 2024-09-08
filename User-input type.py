be93abeadb1cff2424db6b95b5f8deec554df274
TarqueenJ <tarqueentech@gmail.com>
2024-09-08 14:15:59 +0300

    first commit

---
+++ User-input type.py
@@ -0,0 +1,9 @@
+# Asking the user for their name, age, and location
+
+# Getting user inputs
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+# Displaying a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
